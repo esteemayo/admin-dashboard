2fb26a9de4b17e7b83edfcee65863b4c13477e89
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-02 18:18:30 +0100

    feat: import useLocation hook

--- src/components/datatable/DataTable.js
+++ src/components/datatable/DataTable.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { DataGrid } from '@mui/x-data-grid';
+import { Link, useLocation } from 'react-router-dom';
 
 import { userColumns, userRows, productColumns, productRows } from 'data';
 
